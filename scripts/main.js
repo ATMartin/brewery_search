059d76149f04ba3595b21361f912e202c7c8fd12
Alex Martin <martin.alex.t+github@gmail.com>
2015-02-11 00:07:44 -0500

    Fix results route/template

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var AppRouter = Backbone.Router.extend({
     routes: {
       "": "index",
-      // "results/:id": "results",
+      "results/:id": "results",
       // "brewery/:id": "brewery"
     },
 
@@ -24,18 +24,41 @@ var AppRouter = Backbone.Router.extend({
     $('.app-container').html(renderedTemplate);
   },
 
-  // results: function(){
-  //   var template = _.template( $('#brewery-list-template').text() );
-  //   var renderedTemplate = template;
-  //   $('.app-container').html(renderedTemplate);
-  // },
-  //
+  // Let's walk through the second route
+  results: function(id){
+    // This route gets an "id" variable because it has an ":id" variable
+    // in the routes declaration above. So I've added that to the function
+    // declaration here. 
+    // This looks good, but remember - _.template() returns a function.
+    var template = _.template( $('#brewery-list-template').text() );
+    // We need to call this template to make it usable. It also needs
+    // an object with keys to fill your template with. I'm adding the
+    // object, and I'm updating your template with one line so you can see
+    // how that URL/route variable works.
+    var objectForTemplate = { 'index' : 'someRandomVariable', 'urlReference' : id }
+    // Now we call the template function on our new object to actually render
+    // our template (filled in). 
+    var renderedTemplate = template(objectForTemplate);
+    // And we cross our fingers as we hit "refresh". 
+    $('.app-container').html(renderedTemplate);
+  },
+
+  /**
+   * You might see some really wonky rendering with the "results" route above. 
+   * What's happening is that you're rendering both the index route AND the
+   * results route, so both may show up on your page. Nothing major to worry about.
+   * We'll go over soon how this is a good thing, as you may want to render
+   * smaller bits across different parts of the same page. 
+  **/
+  
+  // YOUR TURN! Fix this function like the first two and get it working.
+  // Don't forget to reactivate your route directive up above!
+
   // brewery: function(){
   //   var template = _.template( $('#brewery-info-template').text() );
   //   var renderedTemplate = template;
   //   $('.app-container').html(renderedTemplate);
-  // }
-
+  //}
 
 });
 
