77f8966ba7703354114ddef8d9acf28f8b1d688b
Alex Martin <martin.alex.t+github@gmail.com>
2015-02-10 23:45:57 -0500

    Fix index route/template.

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,22 @@ var AppRouter = Backbone.Router.extend({
       // "brewery/:id": "brewery"
     },
 
+  // Using index as a walkthrough...
   index: function(){
+    // _.template() returns a function. So here, template is a function,
+    // not the actual HTML for your page.
     var template = _.template( $('#brewery-search-template').text() );
-    var renderedTemplate = template;
+    // Since template is a function, we have to invoke it. It expects 
+    // an object of key/value pairs that match the keys in your "beestings"
+    // inside your text/template. Your text/template has a single placeholder
+    // right now: "index". It looks like you wanted this be a search term, 
+    // but that will take some more work. For now I'll just put a dummy
+    // value in.
+    var objectForTemplate = { 'index':'myVariable' };
+    // Now, we call the template function we created with _.template() 
+    // earlier, and we give it an object like it wants.  
+    var renderedTemplate = template(objectForTemplate);
+    // renderedTemplate should be populate now - let's append it. 
     $('.app-container').html(renderedTemplate);
   },
 
